f91e5723faafa4e21c7026dd87e6dd44f80e2a9d
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-03-26 17:18:00 +0100

    Refactor clinical attrs extraction from CSV into a new generic `CSVDataset`
    Also extract functions to modify node link data as networkx utils

--- src/genesis/data/__init__.py
+++ src/genesis/data/__init__.py
@@ -1,4 +1,5 @@
 from .datamodule import LightningDataset, SplitLightningDataset
+from .dataset import CSVDataset
 from .split import k_fold, subsets_split
 
-__all__ = ["LightningDataset", "SplitLightningDataset", "k_fold", "subsets_split"]
+__all__ = ["CSVDataset", "LightningDataset", "SplitLightningDataset", "k_fold", "subsets_split"]
