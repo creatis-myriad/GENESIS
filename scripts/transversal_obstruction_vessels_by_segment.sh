881e7853734f45e20b1b129586aa1a6cb2cb63fb
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-06-13 14:41:54 +0200

    WIP: Scripts to compute transversal obstruction for multiple graphs

---
+++ scripts/transversal_obstruction_vessels_by_segment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INPUT_DIR=$1
+VESSEL_MASKS_DIR=$2
+OBSTRUCTION_MASKS_DIR=$3
+
+for graph_filepath in "${INPUT_DIR}"/*.json; do
+  echo "Processing $graph_filepath"
+  graph_filename=$(basename "$graph_filepath" .json)
+  graph_id=${graph_filename::4}
+  python src/genesis/data/persevere/transversal_obstruction.py \
+    "${VESSEL_MASKS_DIR}/${graph_id}_skeleton_vessels_volume.nii.gz" \
+    "${OBSTRUCTION_MASKS_DIR}/${graph_id}_embolism.nii.gz" \
+    "${graph_filepath}" \
+    --vessel_mask_by_segment
+done
