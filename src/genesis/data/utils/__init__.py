12c1dedc0c71755650c411b7d2ab0370c16ae32f
Sukikui <nonces-cheviotte0o@icloud.com>
2025-07-16 17:02:43 +0200

    feat: add the obstruction attrs adding

--- src/genesis/data/utils/__init__.py
+++ src/genesis/data/utils/__init__.py
@@ -5,6 +5,7 @@ from .networkx import (  # noqa: I001 # must be imported before io package to av
     networkx_remove_attrs,
     networkx_setdefault_attrs,
     networkx_aggregate_list_attrs,
+    networkx_add_obstruction_attribute,
 )
 from .io import json_to_networkx, json_to_pyg, NumpyEncoder, load_nifti, networkx_to_json
 from .sklearn import impute
@@ -16,6 +17,7 @@ __all__ = [
     "json_to_pyg",
     "load_nifti",
     "networkx_add_attrs",
+    "networkx_add_obstruction_attribute",
     "networkx_aggregate_list_attrs",
     "networkx_line_graph",
     "networkx_remove_attrs",
