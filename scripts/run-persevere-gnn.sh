675b7044ddd688d97b6966c5e2dc4e1c09d9f1ee
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-10-01 16:31:02 +0200

    Update bash script sweeping models/targets to log final model checkpoints

--- scripts/run-persevere-gnn.sh
+++ scripts/run-persevere-gnn.sh
@@ -6,7 +6,7 @@ export LOG_DIR=$1
 # not match. This way, the splits computed from the first target will be used for all subsequent targets.
 
 # Multi-class classification risk prediction
-gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb trainer=gpu test=True experiment=persevere/gcn,persevere/gin,persevere/gat data/split=k_fold data/dataset/target=risk_ESC-2014 +data.on_conflict=ignore 'data.split_idx=range(10)' >>"${LOG_DIR}/persevere-gnn-multi_classification.log" 2>&1
+gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb logger.wandb.log_model=True trainer=gpu test=True experiment=persevere/gcn,persevere/gin,persevere/gat data/split=k_fold data/dataset/target=risk_ESC-2014 +data.on_conflict=ignore 'data.split_idx=range(10)' >>"${LOG_DIR}/persevere-gnn-multi_classification.log" 2>&1
 
 # Binary classification elevated bio-markers/risk prediction
-gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb trainer=gpu test=True experiment=persevere/gcn,persevere/gin,persevere/gat data/split=k_fold data/dataset/target=risk_ESC-2014_elevated,troponin_elevated,nt-probnp_elevated,enzymes_elevated +data.on_conflict=ignore model/metrics=binary_classification 'data.split_idx=range(10)' >>"${LOG_DIR}/persevere-gnn-binary_classification.log" 2>&1
+gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb logger.wandb.log_model=True trainer=gpu test=True experiment=persevere/gcn,persevere/gin,persevere/gat data/split=k_fold data/dataset/target=risk_ESC-2014_elevated,troponin_elevated,nt-probnp_elevated,enzymes_elevated +data.on_conflict=ignore model/metrics=binary_classification 'data.split_idx=range(10)' >>"${LOG_DIR}/persevere-gnn-binary_classification.log" 2>&1
