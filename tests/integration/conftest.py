893473b07195bb1cea9780c28568646f51942297
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-10-01 16:24:57 +0200

    Add virtual node (VN) transform + GIN+VN model that uses it

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -200,7 +200,7 @@ def enzymes_classification_overrides() -> list[str]:
     ]
 
 
-@pytest.fixture(scope="package", params=["gcn", "gat", "gin"])
+@pytest.fixture(scope="package", params=["gcn", "gat", "gin", "gin+vn"])
 def graph_level_model_overrides(request: FixtureRequest) -> list[str]:
     """A pytest fixture for the overrides to use to specify the model for the tests.
 
