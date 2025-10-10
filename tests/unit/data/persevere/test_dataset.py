7115e6c8ccddf1b4078cc049364d15f6f70b9648
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-10-10 17:59:37 +0200

    Update `test_persevere_dataset` to expect exceptions on bad features/filters configurations

--- tests/unit/data/persevere/test_dataset.py
+++ tests/unit/data/persevere/test_dataset.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from pathlib import Path
 
 import pytest
@@ -24,18 +25,47 @@ def test_persevere_dataset(
     graph_features_count: int,
 ) -> None:
     """Test PersevereDataset with and without attribute filters and in line graph mode."""
-    dataset = PersevereDataset(
-        root=str(dataset_root),
-        line_graph=line_graph,
-        target_attr="target",
-        node_attrs_filter=node_attrs_filter,
-        edge_attrs_filter=edge_attrs_filter,
-        graph_attrs_filter=graph_attrs_filter,
-        json_to_nx_kwargs={
-            "nodes": NODES_KEY,
-            "edges": EDGES_KEY,
-        },
-    )
+    exc_msg = None
+
+    if (node_attrs_filter and "node" not in dataset_root.name) or (
+        edge_attrs_filter and "edge" not in dataset_root.name
+    ):
+        # If filtering on node or edge attributes but the dataset does not contain any, expect KeyError
+        dataset_init_expectation = pytest.raises(KeyError)
+    elif line_graph and node_attrs_filter:
+        # If filtering on node attributes in line graph mode, expect KeyError because node features are not converted
+        # to edge features
+        dataset_init_expectation = pytest.raises(KeyError)
+    elif graph_attrs_filter and "graph" not in dataset_root.name:
+        # If filtering on graph attributes but the dataset does not contain any, expect AttributeError
+        # The error is different from node/edge attributes because graph attributes are customs and not built-in to PyG
+        dataset_init_expectation = pytest.raises(AttributeError)
+        exc_msg = "'GlobalStorage' object has no attribute"
+    else:
+        # Otherwise, no exceptions are expected to be raised, which can be checked using nullcontext. See pytest doc:
+        # https://docs.pytest.org/en/stable/example/parametrize.html#parametrizing-conditional-raising
+        dataset_init_expectation = nullcontext()
+
+    with dataset_init_expectation as e:
+        dataset = PersevereDataset(
+            root=str(dataset_root),
+            line_graph=line_graph,
+            target_attr="target",
+            node_attrs_filter=node_attrs_filter,
+            edge_attrs_filter=edge_attrs_filter,
+            graph_attrs_filter=graph_attrs_filter,
+            json_to_nx_kwargs={
+                "nodes": NODES_KEY,
+                "edges": EDGES_KEY,
+            },
+        )
+
+    # If dataset initialization should fail, check the expected exception was raised
+    if not isinstance(dataset_init_expectation, nullcontext):
+        assert e.type is dataset_init_expectation.expected_exception
+        if exc_msg:
+            assert exc_msg in str(e.value)
+        return  # Stop test here if dataset initialization failed as expected
 
     if node_features_count and node_attrs_filter is not None:
         node_features_count = len(node_attrs_filter)
