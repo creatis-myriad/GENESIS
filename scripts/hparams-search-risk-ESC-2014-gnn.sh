976dc8410990e24b0f523b4183b507a00e5f8576
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-10-13 19:12:44 +0200

    Add GIN+VN as experiment to optimize in hparams search script

--- scripts/hparams-search-risk-ESC-2014-gnn.sh
+++ scripts/hparams-search-risk-ESC-2014-gnn.sh
@@ -6,7 +6,7 @@ LOG_DIR=${LOG_DIR:-./logs}  # Default to "logs" if LOG_DIR is not set by the use
 # NOTE: The number of parallel jobs is reduced to 4 to avoid CUDA out-of-memory errors, since GraphGPS is more memory-intensive
 gnn-train logger=wandb trainer=gpu hydra/launcher=joblib hydra.launcher.n_jobs=4 hparams_search=gps_risk_ESC-2014 >>"${LOG_DIR}/hparams_search_graph_gps_risk_ESC-2014.log" 2>&1
 
-# Use the same hparams search config for the message passing GNNs (GCN, GIN, GAT), but optimize each model separately
-for experiment in gcn gin gat; do
+# Use the same hparams search config for the message passing GNNs (GCN, GAT, GIN and its variants), but optimize each model separately
+for experiment in gcn gat gin gin+vn; do
   gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb trainer=gpu hparams_search=basic_gnn_risk_ESC-2014 experiment=persevere/$experiment >>"${LOG_DIR}/hparams_search_${experiment}_risk_ESC-2014.log" 2>&1
 done
