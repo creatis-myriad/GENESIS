b4eb95ce354b88f242c65fed31a4cc036a4371f3
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-10-27 11:34:38 +0100

    Implement late fusion of graph-level features w/ graph encoding + Hydra config for LightningModule

--- src/genesis/models/__init__.py
+++ src/genesis/models/__init__.py
@@ -2,6 +2,14 @@ from .abstract import GraphLitModule, MetricTrackingLitModule
 from .baselines.tabular_estimator import TabularEstimator
 from .gnn.gine import GINE
 from .gnn.gps import GPS
-from .graph_level import GraphLevelLitModule
+from .graph_level import GraphLevelLitModule, LateFusionGraphLevelLitModule
 
-__all__ = ["GINE", "GPS", "GraphLevelLitModule", "GraphLitModule", "MetricTrackingLitModule", "TabularEstimator"]
+__all__ = [
+    "GINE",
+    "GPS",
+    "GraphLevelLitModule",
+    "GraphLitModule",
+    "LateFusionGraphLevelLitModule",
+    "MetricTrackingLitModule",
+    "TabularEstimator",
+]
