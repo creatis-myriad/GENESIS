a416a8704863f1a65325d83b4011604f7a79d81c
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-11-05 18:42:14 +0100

    Add GAGPS experiments to hparams search and PERSEVERE sweeps bash scripts

--- scripts/hparams-search-persevere.sh
+++ scripts/hparams-search-persevere.sh
@@ -22,10 +22,12 @@ for model in gcn gat gin; do
   done
 done
 
-# Use a dedicated hparams search config for GPS, because it has different hyperparameters than the basic GNNs
-gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
-  hparams_search=persevere_gps experiment=persevere/"${TARGET}"/gps \
-  +data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_gps_${TARGET}.log" 2>&1
+# Use a dedicated hparams search config for GPS and its variants, because they have different hyperparameters than the basic GNNs
+for model in gps gagps; do
+  gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
+    hparams_search=persevere_gps experiment=persevere/"${TARGET}"/${model} \
+    +data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_${model}_${TARGET}.log" 2>&1
+done
 
 # Separate loop for MLP baseline, since its variants are different from other GNNs
 model=mlp
