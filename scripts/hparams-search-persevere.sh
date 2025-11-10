87ae788c58d4f098d3f7d753a298cf0743e25c97
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-11-10 12:51:43 +0100

    Fix GPS command in hparams search bash script
    * Avoid forcing binary classification metrics, to let individual experiment configs handle it
    * Increased the nb of parallel jobs from 4 to the default 10 used for other models, since previous fix reduced GPS' memory footprint

--- scripts/hparams-search-persevere.sh
+++ scripts/hparams-search-persevere.sh
@@ -14,5 +14,4 @@ for model in gcn gat gin; do
 done
 
 # Use a dedicated hparams search config for GPS, because it has different hyperparameters than the basic GNNs
-# NOTE: The number of parallel jobs is reduced to 4 to avoid CUDA out-of-memory errors, since GPS is more memory-intensive
-gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=4 logger=wandb trainer=gpu hparams_search=persevere_gps experiment=persevere/"${TARGET}"/gps +data.on_conflict=ignore model/metrics=binary_classification >>"${LOG_DIR}/hparams_search_gps_${TARGET}.log" 2>&1
+gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 logger=wandb trainer=gpu hparams_search=persevere_gps experiment=persevere/"${TARGET}"/gps +data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_gps_${TARGET}.log" 2>&1
