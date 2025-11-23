3e4fedb64e2f14ffb39dfe33698c0764e75338c3
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-11-23 20:29:33 +0100

    Add elevated risk as target option in PERSEVERE model sweep bash script
    As elevated risk was not one of the targets for hparams search, it is configured to use hparams optimized for multiclass risk

--- scripts/run-persevere-gnn.sh
+++ scripts/run-persevere-gnn.sh
@@ -9,8 +9,15 @@ targets_configs=(
   [enzymes_elevated]="enzymes_elevated"
   # Targets for which optimized hparams are not available,
   # in which case we default to optimized hparams from a compatible target
+  [risk_ESC-2014_elevated]="risk_ESC-2014"
   [troponin_elevated]="enzymes_elevated"
   [nt-probnp_elevated]="enzymes_elevated"
+)
+declare -A targets_overrides
+targets_overrides=(
+  # Since the binary `risk_ESC-2014_elevated` targets uses the config from the multiclass `risk_ESC-2014` target,
+  # it must override multiclass metrics config with binary config
+  [risk_ESC-2014_elevated]="model/metrics=binary_classification"
 )
 models=(
   "mlp" "gcn" "gat" "gin" "gps"
@@ -28,9 +35,11 @@ for target in "${!targets_configs[@]}"; do # Loop over targets and associated
   for model in "${models[@]}"; do  # Loop over models
     # NOTE: Ignore conflicts on data splits (+data.on_conflict=ignore), because splits computed from different targets would
     # not match. This way, the splits computed from the first target will be used for all subsequent targets.
+    # shellcheck disable=SC2086
     gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu \
       logger=wandb logger.wandb.log_model=True test=True \
       experiment=persevere/"${experiment_config_group}"/"${model}" \
+      ${targets_overrides[${target}]} \
       data/dataset/target="${target}" data/split=k_fold +data.on_conflict=ignore 'data.split_idx=range(10)' \
       >>"${LOG_DIR}/persevere-gnn-${target}-${model}.log" 2>&1
   done
