d7a2f44a043132acb4ba635977347a392acf4846
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-12-02 20:30:22 +0100

    Add doctest for `GraphAttrData` class

--- src/genesis/data/data.py
+++ src/genesis/data/data.py
@@ -12,6 +12,18 @@ class GraphAttrData(Data):
     References:
         - This is the recommended way to implement this behavior, as per PyG docs:
           https://pytorch-geometric.readthedocs.io/en/latest/advanced/batching.html#batching-along-new-dimensions
+
+    Examples:
+        >>> import torch
+        >>> from torch_geometric.data import Batch
+        >>> num_graphs = 4
+        >>> num_graph_features = 10
+        >>> data_list = [GraphAttrData(graph_attr=torch.randn(num_graph_features)) for _ in range(num_graphs)]
+        >>> data_list[0].graph_attr.shape  # (num_graph_features,)
+        torch.Size([10])
+        >>> batch = Batch.from_data_list(data_list)
+        >>> batch.graph_attr.shape  # (num_graphs, num_graph_features)
+        torch.Size([4, 10])
     """
 
     def __cat_dim__(self, key: str, value: Any, *args, **kwargs) -> Any:  # noqa: D105
