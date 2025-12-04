deab3b5c02ce848ed8c088b1194efa64e832c4ac
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-12-04 15:37:33 +0100

    Make options used for tabular sweep in bash script more consistent with GNN sweep script

--- scripts/run-persevere-tabular.sh
+++ scripts/run-persevere-tabular.sh
@@ -7,14 +7,12 @@ LOG_DIR=${1:-./logs}  # Default to "logs" if LOG_DIR is not set by the user
 
 # Fit and score tabular baselines (model/model) across multiple targets (experiments) and feature sets (data/dataset/usecols)
 # shellcheck disable=SC2016
-baseline-tabular -m \
-  logger=wandb \
-  test=True \
-  data/split=k_fold \
+baseline-tabular -m hydra/launcher=joblib hydra.launcher.n_jobs=10 \
+  logger=wandb test=True \
   experiment=tabular_baseline/risk_ESC-2014,tabular_baseline/risk_ESC-2014_elevated,tabular_baseline/enzymes_elevated \
   data/dataset/usecols=spesi,spesi+cardiac_biomarkers,spesi+graph_biomarkers,spesi+cardiac_biomarkers+graph_biomarkers \
-  +data.on_conflict=ignore \
   model/model=tabpfn,xgboost \
+  data/split=k_fold +data.on_conflict=ignore \
   'data.split_idx=range(10)' \
   'ckpt_backbone_save_dirpath="${paths.ckpt_dir}/${op:call,${op:methodcaller,upper},${hydra:runtime.choices.data/dataset}}/${hydra:runtime.choices.data/dataset/target}/${hydra:runtime.choices.model/model}/${hydra:runtime.choices.data/dataset/usecols}/${hydra:runtime.choices.data/split}/${data.split_idx}"' \
   'ckpt_backbone_save_filename="${op.ternary:${op:eq,${hydra:runtime.choices.model/model},tabpfn},model.tabpfn_fit,null}"' \
