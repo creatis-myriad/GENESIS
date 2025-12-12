ca46ead51fe1eb6cc3ba386cd5ef6162cfa96113
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-12-12 17:20:03 +0100

    Add Hydra experiment configs + bash script for multimodal GNNs on MolHIV dataset

--- scripts/run-generalization.sh
+++ scripts/run-generalization.sh
@@ -2,20 +2,23 @@
 
 LOG_DIR=${1:-./logs}  # Default to "logs" if LOG_DIR is not set by the user
 
+# Map datasets to their root folder names under the data root directory
+declare -A datasets
 datasets=(
-  "zinc"
+  [zinc]="ZINC"
+  [molhiv]="ogbg_molhiv"
 )
 models=(
   "gine+vn" "gine+vn+ef" "gine+vn+lf" "gine+gavn"
   "gps" "gps+ef" "gps+lf" "gps+xa" "gps+ftxa"
 )
 
-for dataset in "${datasets[@]}"; do # Loop over datasets
+for dataset in "${!datasets[@]}"; do # Loop over datasets
   for model in "${models[@]}"; do  # Loop over models
     # Clear cached processed data manually, instead of using `data.dataset.force_reload=True` option
     # This has the same effect as `force_reload` of avoiding errors between models requiring different pre-processing,
     # while avoiding re-processing datasets between different random seeds for the same model.
-    rm -rf ./data/"${dataset^^}"/**/processed
+    rm -rf ./data/"${datasets[${dataset}]}"/**/processed >>"${LOG_DIR}/generalization-${dataset}-${model}.log" 2>&1
     gnn-train -m trainer=gpu \
       logger=wandb test=True \
       experiment=generalization/"${dataset}"/"${model}" \
