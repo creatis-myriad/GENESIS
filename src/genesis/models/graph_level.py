3fb6f3a0ab592c8f0dff48623727bb8b36d2fd14
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-12-12 09:26:30 +0100

    Fix to handling of edge features in special case when they have size of 1
    - Add feature dim of size 1 to `edge_attr` in model forward pass to match general case of multi-dimensional features
    - When generating `example_input_array` using `FakeDataset`, copy features assigned to `edge_weight` to the expected `edge_attr` attribute
    
    Also refactors how attributes are manually updated on `FakeDataset` to operate on underlying `Data` objects instead. This avoids changes silently not being reflected on final batched results because of cached list not being updated in `FakeDataset`.

--- src/genesis/models/graph_level.py
+++ src/genesis/models/graph_level.py
@@ -60,6 +60,12 @@ class GraphLevelLitModule(GraphLitModule):
         Returns:
             The predicted logits for the input graphs in the batch.
         """
+        if data.edge_attr is not None and data.edge_attr.ndim == 1:
+            # If edge features exist but are 1D (e.g. ZINC dataset), add feature dimension of size 1 at the end to fit
+            # in the general case of edge features with multiple dimensions, and tobe compatible with models expecting
+            # edge features to have shape [num_edges, num_edge_features]
+            data.edge_attr = data.edge_attr.unsqueeze(-1)
+
         data = self.transforms(data) if self.transforms is not None else data
         x = self._encoder_step(data, data.x.float())  # Ensure graph node features are float
         x = self._readout_step(data, x)
