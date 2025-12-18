37d485a0bf7f905ee180c557b8a9288057511288
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2025-12-18 10:44:23 +0100

    Move and rename `tabular_estimator` module as `estimator` module in `tabular` package

--- src/genesis/models/__init__.py
+++ src/genesis/models/__init__.py
@@ -1,9 +1,9 @@
 from .abstract import GraphLitModule, MetricTrackingLitModule
-from .baselines.tabular_estimator import TabularEstimator
 from .gnn.gine import GINE
 from .gnn.gps import GPS
 from .gnn.vn_g import GAT_VN_G, GCN_VN_G, GIN_VN_G
 from .graph_level import GPSGraphLevelLitModule, GraphLevelLitModule, LateFusionGraphLevelLitModule
+from .tabular.estimator import TabularEstimator
 
 __all__ = [
     "GAT_VN_G",
