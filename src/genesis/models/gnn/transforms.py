0e468429f373f8a0d120aa58c99a2d91037916f0
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2026-01-13 23:51:47 +0100

    Add learnable `VirtualNodes` for batched data, enabling chaining in model transforms
    Also refactor `GraphAttributesVirtualNodes` to avoid redundant code with the new, more general `VirtualNodes`

--- src/genesis/models/gnn/transforms.py
+++ src/genesis/models/gnn/transforms.py
@@ -5,14 +5,43 @@ from torch.nn import Linear
 from torch_geometric.data import Batch, Data
 from torch_geometric.transforms import BaseTransform
 
-from genesis.data.transforms.virtual_node import VirtualNodes
+from genesis.data.transforms.virtual_node import VirtualNodes as VirtualNodesPerGraph
 
 
 class LearnableTransform(BaseTransform, nn.Module, ABC):
     """A base class for transformations with learnable parameters."""
 
 
-class GraphAttributesVirtualNodes(LearnableTransform):
+class VirtualNodes(LearnableTransform):
+    """Wrapper around our `VirtualNodes` to fit into the `LearnableTransform` API and apply to batched graphs.
+
+    Inherits from `nn.Module` indirectly through `LearnableTransform`, even though it contains no learnable parameters,
+    which enables chaining with other learnable transforms inside collections like `nn.Sequential` or `nn.ModuleDict`.
+    """
+
+    def __init__(self) -> None:
+        """Initializes a `GraphAttributesVirtualNodes` instance."""
+        super().__init__()
+        self.virtual_nodes = VirtualNodesPerGraph()
+
+    def forward(self, data: Data) -> Data:  # noqa: D102
+        data_list = data.to_data_list() if (is_batch := isinstance(data, Batch)) else [data]
+        new_data_list = [self._add_virtual_nodes_to_graph(graph) for graph in data_list]
+        return Batch.from_data_list(new_data_list) if is_batch else new_data_list[0]
+
+    def _add_virtual_nodes_to_graph(self, graph: Data) -> Data:
+        """Adds virtual nodes to a single graph.
+
+        Args:
+            graph: The input graph to which to add virtual node(s).
+
+        Returns:
+            Input graph with virtual node(s) added.
+        """
+        return self.virtual_nodes.forward(graph)
+
+
+class GraphAttributesVirtualNodes(VirtualNodes):
     """Appends a virtual node (i.e. connected to all nodes) initialized with an embedding learned from graph attributes.
 
     This implementation was made to support multimodal data (i.e. graph attributes) as input features of virtual nodes.
@@ -27,19 +56,11 @@ class GraphAttributesVirtualNodes(LearnableTransform):
                 like positional encoding have been applied).
         """
         super().__init__()
-        self.virtual_nodes = VirtualNodes()
         self.graph_lin = Linear(graph_dim, embed_dim)
 
-    def forward(self, data: Data) -> Data:  # noqa: D102
-        data_list = data.to_data_list() if (is_batch := isinstance(data, Batch)) else [data]
+    def _add_virtual_nodes_to_graph(self, graph: Data) -> Data:
+        # Project graph features to embedding dimension
+        graph_attr_embedding = self.graph_lin(graph.graph_attr)
 
-        new_data_list = []
-        for graph in data_list:
-            # Project graph features to embedding dimension
-            graph_attr_embedding = self.graph_lin(graph.graph_attr)
-
-            # Add virtual node initialized with graph attribute embedding
-            graph_with_vn = self.virtual_nodes.forward(graph, init_values=graph_attr_embedding)
-            new_data_list.append(graph_with_vn)
-
-        return Batch.from_data_list(new_data_list) if is_batch else new_data_list[0]
+        # Add virtual node initialized with graph attribute embedding
+        return self.virtual_nodes.forward(graph, init_values=graph_attr_embedding)
