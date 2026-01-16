4ee35aabe05213becf2ee0d2a0f4fdaa1241dd4c
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2026-01-16 22:48:05 +0100

    In molecular experiments script, uniquely override `paths.data_dir` per run to enable parallel jobs on the same dataset
    Since we no longer need to know the dataset root path, simplify loop over datasets

--- scripts/run-generalization.sh
+++ scripts/run-generalization.sh
@@ -2,27 +2,27 @@
 
 LOG_DIR=${1:-./logs}  # Default to "logs" if LOG_DIR is not set by the user
 
-# Map datasets to their root folder names under the data root directory
-declare -A datasets
 datasets=(
-  [zinc]="ZINC"
-  [molhiv]="ogbg_molhiv"
+  "zinc" "molhiv"
 )
 models=(
-  "gine+vn" "gine+vn+ef" "gine+vn+lf" "gine+gavn"
-  "gps" "gps+ef" "gps+lf" "gps+xa" "gps+ftxa"
+  "gine+vn"
+  "gine+vn+ef" "gine+vn+lf" "gine+gavn"
+  "gps"
+  "gps+ef" "gps+lf" "gps+xa" "gps+ftxa"
 )
 
 shopt -s globstar # Enable ** globbing, disabled by default in bash (see https://stackoverflow.com/a/62985520)
 
-for dataset in "${!datasets[@]}"; do # Loop over datasets
+for dataset in "${datasets[@]}"; do # Loop over datasets
   for model in "${models[@]}"; do  # Loop over models
-    # Clear cached processed data manually, instead of using `data.dataset.force_reload=True` option
-    # This has the same effect as `force_reload` of avoiding errors between models requiring different pre-processing,
-    # while avoiding re-processing datasets between different random seeds for the same model.
-    rm -rf ./data/"${datasets[${dataset}]}"/**/processed
-    gnn-train -m trainer=gpu data.num_workers=6 \
+    # Set `paths.data_dir` to a unique folder per run to avoid conflicts when different models require different
+    # pre-processing, and to avoid race conditions between different seeds that could use the same dataset but are
+    # run in parallel. This speeds up total experiments runtime, at the cost of using more disk space.
+    # shellcheck disable=SC2016,SC2086
+    gnn-train -m hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu \
       logger=wandb test=True \
+      paths.data_dir='${.root_dir}'/data/duplicates4parallel_jobs/${model}/'${seed}' \
       experiment=generalization/"${dataset}"/"${model}" \
       'seed=range(10)' \
       >>"${LOG_DIR}/generalization-${dataset}-${model}.log" 2>&1
