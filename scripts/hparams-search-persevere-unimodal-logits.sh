565f5f204d2e3dbb6f33b38d15ca5ec4dc71d27a
Nathan Painchaud <nathan.painchaud@insa-lyon.fr>
2026-03-24 09:27:21 +0100

    WIP: Add GNN hparams search config variants compatible with unimodal logits head

---
+++ scripts/hparams-search-persevere-unimodal-logits.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+TARGET=${1:-"risk_ESC-2014"}  # Default to "risk_ESC-2014" if TARGET is not set by the user
+LOG_DIR=${2:-./logs}  # Default to "logs" if LOG_DIR is not set by the user
+
+# NOTE: Ignore conflicts on data splits (data.on_conflict=ignore), because splits computed from different targets would
+# not match. This way, pre-computed splits will be used if available, otherwise splits will be computed for the target.
+
+# Use the same hparams search config for the message passing GNNs (GCN, GAT, GIN) and their variants, but optimize each configuration separately
+#for model in gcn gat gin; do
+# shellcheck disable=SC2043
+for model in gin; do
+#  for variant in "" +vn +ef +vn+ef +lf +vn+lf +gavn; do
+  # shellcheck disable=SC2043
+  for variant in +gavn; do
+    gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
+      hparams_search=persevere_basic_gnn experiment=persevere/"${TARGET}"/${model}${variant} \
+      model/head=unimodal_logits ~model.head.num_layers \
+      logger.wandb.project=GENESIS-ordinal-regression-hparams-search \
+      data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_${model}${variant}_${TARGET}.log" 2>&1
+  done
+
+  # Use a dedicated hparams search config for VN_G variants, because they have different hyperparameters than the other variants
+#  for variant in +vn_g +vn_gv2; do
+#    gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
+#      hparams_search=persevere_vn_g experiment=persevere/"${TARGET}"/${model}${variant} \
+#      data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_${model}${variant}_${TARGET}.log" 2>&1
+#  done
+done
+
+# Use dedicated hparams search configs for GPS and its extensions/variants, because they have different hyperparameters
+declare -A gps_hparams_search_configs
+# Associative mapping between GPS (variant) and the hparams search config to use
+gps_hparams_search_configs=(
+#  [gps]="persevere_gps"
+#  [gps+ef]="persevere_gps"
+#  [gps+lf]="persevere_gps"
+#  [gps+xa]="persevere_gps+ga"
+  [gps+ftxa]="persevere_gps+ga"
+)
+for model in "${!gps_hparams_search_configs[@]}"; do
+  gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
+    hparams_search="${gps_hparams_search_configs[${model}]}" experiment=persevere/"${TARGET}"/"${model}" \
+    model/head=unimodal_logits ~model.head.num_layers \
+    logger.wandb.project=GENESIS-ordinal-regression-hparams-search \
+    data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_${model}_${TARGET}.log" 2>&1
+done
+
+# Separate loop for MLP baseline, since its variants are different from other GNNs
+#model=mlp
+#for variant in "" +ef +lf; do
+#  gnn-train hydra/launcher=joblib hydra.launcher.n_jobs=10 trainer=gpu logger=wandb \
+#    hparams_search=persevere_basic_gnn experiment=persevere/"${TARGET}"/${model}${variant} \
+#    data.on_conflict=ignore >>"${LOG_DIR}/hparams_search_${model}${variant}_${TARGET}.log" 2>&1
+#done
